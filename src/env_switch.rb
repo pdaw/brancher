ad720919c2d83ae859d2912d72647c07caec0a92
Piotr Dawidiuk <piotr.dawidiuk@gmail.com>
2016-05-27 21:44:50 +0200

    git commands

--- src/env_switch.rb
+++ src/env_switch.rb
@@ -1,15 +1,18 @@
 require_relative 'repositories_factory'
 require_relative 'files_synchronization'
+require_relative 'git_checkout'
 
 class EnvSwitch
   def initialize
     @files_synchronization = FilesSynchronization.new
+    @git_checkout = GitCheckout.new
   end
 
   def switch
     repositories_factory = RepositoriesFactory.new
     repositories = repositories_factory.create
     repositories.each do |repository|
+      @git_checkout.checkout_branch(repository)
       @files_synchronization.synchronize(repository)
     end
   end
--- src/git_checkout.rb
+++ src/git_checkout.rb
@@ -1,2 +1,9 @@
+require 'minigit'
+
 class GitCheckout
+  def checkout_branch(repository)
+    @git_client = MiniGit.new(repository.local_path)
+    @git_client.fetch
+    @git_client.checkout(repository.branch_name)
+  end
 end
