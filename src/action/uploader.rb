57dfd77c5757fc283a51381797f1a7fb5c9e0c94
Piotr Dawidiuk <piotr.dawidiuk@gmail.com>
2016-05-29 00:03:37 +0200

    removal of hardcoded blacklist

--- src/action/uploader.rb
+++ src/action/uploader.rb
@@ -2,18 +2,15 @@ require 'net/ssh'
 require 'net/scp'
 
 class Uploader
-  def initialize
-    @files_blacklist = %w(.git . .. vendor bin)
-  end
-
   def upload(repository)
     host = repository.host
     local_path = repository.local_path
     commands = repository.commands
+    blacklist = repository.blacklist
 
     Net::SSH.start(host.server, host.user, :password => host.password, :port => host.port) do |ssh|
       scp_client = Net::SCP.new(ssh)
-      files_to_upload = get_files_to_upload(local_path)
+      files_to_upload = get_files_to_upload(local_path, blacklist)
       files_to_upload.each do |file|
         scp_client.upload!(local_path + "/#{file}", host.remote_path, :recursive => true) do |ch, name, sent, total|
           puts "#{name}: #{sent}/#{total}"
@@ -28,10 +25,10 @@ class Uploader
 
   private
 
-  def get_files_to_upload(path)
+  def get_files_to_upload(path, blacklist)
     files_to_upload = Dir.entries(path)
 
-    @files_blacklist.each do |file|
+    blacklist.each do |file|
       files_to_upload.delete(file)
     end
 
